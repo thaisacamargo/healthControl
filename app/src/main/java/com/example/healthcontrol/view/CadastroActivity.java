97f4c6ec1eba7fbd735a6e1ac94717d75270b9f3
thaisacamargo <thais.camargo.ta@gmail.com>
2019-09-21 10:03:41 -0300

    Inclusão do xml

--- app/src/main/java/com/example/healthcontrol/view/CadastroActivity.java
+++ app/src/main/java/com/example/healthcontrol/view/CadastroActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.healthcontrol.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
     }
 }
--- app/src/main/java/com/example/healthcontrol/view/Resultado.java
+++
@@ -1,4 +0,0 @@
-package com.example.healthcontrol.view;
-
-public class Resultado {
-}
