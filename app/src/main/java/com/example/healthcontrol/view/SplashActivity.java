48ba04d069a515aba385b0111349813c24663e5d
andrearibas <Andrearibas>
2019-09-21 10:58:05 -0300

    Splash

--- app/src/main/java/com/example/healthcontrol/view/SplashActivity.java
+++ app/src/main/java/com/example/healthcontrol/view/SplashActivity.java
@@ -2,15 +2,47 @@ package com.example.healthcontrol.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.healthcontrol.R;
 
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imagemSplash;
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imagemSplash = findViewById(R.id.imagemSplash_view);
+
+        imagemSplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jump();
+            }
+        });
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+            }
+        },3000);
+    }
+
+    private void jump(){
+        timer.cancel();
+        startActivity(new Intent(SplashActivity.this, CadastroActivity.class));
+        finish();
     }
 }
